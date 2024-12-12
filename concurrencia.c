a4158dd1d534e4aa7f8052b40a1a9318d1487f8c
Jiasheng Liu <125604000+LiuNotAnAngel@users.noreply.github.com>
2024-12-12 18:58:20 +0100

    Update concurrencia.c

--- concurrencia.c
+++ concurrencia.c
@@ -3,42 +3,59 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
-#define NUM_THREADS      10
+#define NUM_THREADS 10
 #define MAX_COUNT 1000000
 
-//Variable global mutex para controlar acceso a sección crítica
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;  //macro de inicialización
+// Variable global mutex para controlar acceso a sección crítica
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; // macro de inicialización
 
-//La función que ejecutará cada thread
-void *worker(void *ptr) {
+// La función que ejecutará cada thread
+void *worker(void *ptr)
+{
+    int my_id = getpid(); 
+    printf("Thread ID: %d ejecutando\n", my_id);
+
     int aux;
     for (size_t i = 0; i < MAX_COUNT; i++)
     {
         pthread_mutex_lock(&mutex);
         aux = my_stack_pop();
+        printf("Thread ID: %d ejecutando pop\n", my_id); //Mostrar el mensaje cuando está haciendo pruebas pequeñas
         pthread_mutex_unlock(&mutex);
-        aux = aux+1;
+        aux = aux + 1;
         pthread_mutex_lock(&mutex);
+        printf("Thread ID: %d ejecutando pop\n", my_id); //Mostrar el mensaje cuando está haciendo pruebas pequeñas
         my_stack_push();
         pthread_mutex_unlock(&mutex);
     }
     pthread_exit(NULL);
-
 }
 
-int main (int argc, char *argv[]) {
-    pthread_t threads[NUM_THREADS];
+int main(int argc, char *argv[])
+{
+    pthread_t threads[NUM_THREADS]; //Los id de los threads
 
-    for(i=0; i<NUM_THREADS; i++){
-        pthread_create(&threads[i], NULL, worker, NULL); // 1st arg es ID de thread y 3rd es la función que ejecuta el thread.
+    for (size_t i = 0; i < NUM_THREADS; i++) //Bucle para crear los threads, 1st arg es el id y el 3rd es la función que ejecutará el thread
+    {
+        if (pthread_create(&threads[i], NULL, worker, NULL) != 0) 
+        {
+            perror("Error creando el hilo");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    for(i=0; i<NUM_THREADS; i++){ //El main thread espera hasta que acaben todos los threads
-        pthread_join(threads[i], NULL);
+    for (size_t i = 0; i < NUM_THREADS; i++) //Bucle para que el main thread quede bloqueado hasta que acaben todos los threads. 
+    {
+        if (pthread_join(threads[i], NULL) != 0)
+        {
+            perror("Error uniendo el hilo");
+            exit(EXIT_FAILURE);
+        }
     }
 
-    my:stack_write(); //Guarda la pila modificada en un fichero
-    my_stack_purge(); //Eliminar 
+    my_stack_write(); // Guarda la pila modificada en un fichero
+    my_stack_purge(); // Eliminar
     pthread_exit(NULL);
     return 0;
+    
 }
